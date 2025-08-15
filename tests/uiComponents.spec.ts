7cc6883beb48dd10ce1cde8c486c5cdb7a872c4c
Gaston Fernando Cattani <gcattani@ext.laligatech.com>
2025-08-15 17:04:12 -0300

    Sliders

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -161,22 +161,47 @@ test('datepicker', async ({ page }) => {
     await calendarInputField.click()
 
     let date = new Date()
-    date.setDate(date.getDate() + 10000)
+    date.setDate(date.getDate() + 50)
     const expectedDate = date.getDate().toString()
-    const expectedMonthShort = date.toLocaleString('En-US', {month: 'short'})
-    const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'})
+    const expectedMonthShort = date.toLocaleString('En-US', { month: 'short' })
+    const expectedMonthLong = date.toLocaleString('En-US', { month: 'long' })
     const expectedYear = date.getFullYear()
     const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
     console.log(dateToAssert)
 
     let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
     const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `
-    while(!calendarMonthAndYear.includes(expectedMonthAndYear)) {
+    while (!calendarMonthAndYear.includes(expectedMonthAndYear)) {
         await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
         calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
     }
 
     await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, { exact: true }).click()
     await expect(calendarInputField).toHaveValue(dateToAssert)
-    
 })
+
+test('Sliders', async ({ page }) => {
+    //Update attribute
+    // const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+    // await tempGauge.evaluate(node => {
+    //     node.setAttribute('cx', '232.630')
+    //     node.setAttribute('cy', '232.630')
+    // })
+
+    // await tempGauge.click()
+
+    //Mouse movement
+    const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+    await tempBox.scrollIntoViewIfNeeded()
+
+    const box = await tempBox.boundingBox()
+    const x = box.x + box.width / 2
+    const y = box.y + box.height / 2
+    await page.mouse.move(x, y)
+    await page.mouse.down()
+    await page.mouse.move(x + 100, y)
+    await page.mouse.move(x + 100, y + 100)
+    await page.mouse.up()
+    await expect(tempBox).toContainText('30')
+})
+
