86bd20b1763a0d1612d2c4121a7cc2fb62a92f8d
Gaston Fernando Cattani <gcattani@ext.laligatech.com>
2025-08-15 09:09:33 -0300

    Installed playwright and added tests files

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,133 @@
+import { expect, test } from '@playwright/test';
+
+test.beforeAll(() => { })
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Locator syntax rules', async ({ page }) => {
+    //by tag name
+    await page.locator('input').first().click()
+
+    //by id
+    page.locator('#inputEmail1')
+
+    //by class value
+    page.locator('.shape-rectangle')
+
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class value (full value)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"].shape-rectangle')
+
+    //by XPath (NOT RECOMENDED)
+    page.locator('//input[@id="inputEmail1"]')
+
+    //by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
+
+})
+
+test('User facing locators', async ({ page }) => {
+    //by role
+    await page.getByRole('textbox', { name: "Email" }).first().click()
+    await page.getByRole('button', { name: "Sign In" }).first().click()
+
+    //by label
+    await page.getByLabel('Email').first().click()
+
+    //by placeholder
+    await page.getByPlaceholder('Jane Doe').click()
+
+    //by text
+    await page.getByText('Using the Grid').click()
+
+    //by title (data-testid attribute configurable in playwright.config.ts)
+    await page.getByTitle('IoT Dashboard').click()
+
+    //by test id
+    // await page.getByTestId('test-id-123').click()
+})
+
+test('Locating child elements', async ({ page }) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', { name: "Sign In" }).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+})
+
+test('Locating parent elements', async ({ page }) => {
+    await page.locator('nb-card', { hasText: "Using the Grid" }).getByRole('textbox', { name: "Email" }).click()
+    await page.locator('nb-card', { has: page.locator('#inputEmail1') }).getByRole('textbox', { name: "Email" }).click()
+
+    await page.locator('nb-card').filter({ hasText: "Basic form" }).getByRole('textbox', { name: "Email" }).click()
+    await page.locator('nb-card').filter({ has: page.locator('.status-danger') }).getByRole('textbox', { name: "Password" }).click()
+
+    await page.locator('nb-card').filter({ has: page.locator('nb-checkbox') }).filter({ hasText: "Sign in" })
+        .getByRole('textbox', { name: "Email" }).click()
+
+    //Move to parent element, onlt way using XPath
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', { name: "Email" }).click()
+})
+
+test('Reusing the locators', async ({ page }) => {
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic form" })
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', { name: "Password" }).fill('Welcome123')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
+})
+
+test('Extracting values', async ({ page }) => {
+    //Single text value
+    const basicForm = page.locator('nb-card').filter({ hasText: "Basic form" })
+    const buttonText = await basicForm.locator('button').textContent()
+    expect(buttonText).toEqual('Submit')
+
+    //All text values
+    const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonsLabels).toContain("Option 1")
+
+    //Input value
+    const emailField = basicForm.getByRole('textbox', { name: "Email" })
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeHolderValue = await emailField.getAttribute('placeholder')
+    expect(placeHolderValue).toEqual('Email')
+})
+
+test('Assertions', async ({ page }) => {
+    const basicFormButton = page.locator('nb-card').filter({ hasText: "Basic form" }).locator('button')
+
+    //General assertions
+    const value = 5;
+    expect(value).toEqual(5)
+
+    const text = await basicFormButton.textContent()
+    expect(text).toEqual('Submit')
+
+    //Location assertion
+    await expect(basicFormButton).toHaveText('Submit')
+
+    //Soft Assertion
+    await expect.soft(basicFormButton).toHaveText('Submit')
+    await basicFormButton.click()
+})
---
+++ tests/uiComponents.spec.ts
@@ -0,0 +1,103 @@
+import { InputModalityDetector } from '@angular/cdk/a11y';
+import { expect, test } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test.describe('Form Layouts page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layout').click()
+    })
+
+    test('Input fields', async ({ page }) => {
+        const usingTheGriidEmailInput = page.locator('nb-card', { hasText: "Using the Grid" }).getByRole("textbox", { name: "Email" })
+
+        await usingTheGriidEmailInput.fill('test@test.com')
+        await usingTheGriidEmailInput.clear()
+        await usingTheGriidEmailInput.pressSequentially('test2@test.com', { delay: 500 })
+
+        //Generic assertion
+        const inputValue = await usingTheGriidEmailInput.inputValue()
+        expect(inputValue).toEqual('test2@test.com')
+
+        //Locator assertion
+        await expect(usingTheGriidEmailInput).toHaveValue('test2@test.com')
+    })
+
+    test('Radio buttons', async ({ page }) => {
+        const usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid" })
+
+        // await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', { name: "Option 1" }).check({ force: true })
+        const radioStatus = await usingTheGridForm.getByRole('radio', { name: "Option 1" }).isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', { name: "Option 1" })).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', { name: "Option 2" }).check({ force: true })
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 1" }).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 2" }).isChecked()).toBeTruthy()
+    })
+})
+
+test('Checkboxes', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', { name: "Hide on click" }).uncheck({ force: true })
+    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" }).check({ force: true })
+
+    const allBoxes = page.getByRole('checkbox')
+    for (const box of await allBoxes.all()) { //Uso el .all() para convertirlo en un array
+        await box.uncheck({ force: true })
+        expect(await box.isChecked()).toBeFalsy()
+    }
+})
+
+test('Lists and dropdowns', async ({ page }) => {
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    page.getByRole('list') // When the list has a UL tag
+    page.getByRole('listitem') // When the list has LI tag
+
+    //const optionList = page.getByRole('list').locator('nb-option')
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({ hasText: "Cosmic" }).click()
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        "light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+
+    await dropDownMenu.click()
+    for (const color in colors) {
+        await optionList.filter({ hasText: color }).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if(color != "Corporate")
+            await dropDownMenu.click()
+    }
+})
+
+test('Toolptips', async({page})=>{
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Tooltip').click()
+
+        const toolTipCard = page.locator('nb-card', {hasText: "Tooltip Placements"})
+        await toolTipCard.getByRole('button', {name: "Top"}).hover()
+
+        //page.getByRole('tooltip') // IF you have a role tooltip created
+        const tooltip = await page.locator('nb-tooltip').textContent()
+        expect(tooltip).toEqual("This is a tooltip")
+})
+
+test('Dialog box', async({page}) => {
+        await page.getByText('Tables & Data').click()
+        await page.getByText('Smart Table').click()
+})
